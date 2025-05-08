30db3a0f242c2f176104afb2007b8f69a473cc74
giovannivicentin <giovannifvicentin@gmail.com>
2025-05-08 20:26:40 -0300

    feat: setup the boilerplate for the project with shadcn and nextjs

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en/practices',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
